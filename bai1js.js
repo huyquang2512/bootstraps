21d9afee2906973ba65fc3f3b3d94d02084c1514
huyquang2512 <huylqph07203@fpt.edu.vn>
2021-01-13 00:12:00 +0700

    Bai tap bootstrap[C

---
+++ bai1js.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.header').height($(window).height());
+    /*scroll effect*/
+    $(".navbar a").on('click', function(event) {
+        var position = $("#" + $(this).data('value')).offset().top;
+        $("html, body").animate({ scrollTop: position }, 1000);
+    })
+})
